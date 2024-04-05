3ccc3b1b54b0ad07d539436164cc68524cc8e9ee
aasmitadhakal <dhakalreshma52@gmail.com>
2024-04-05 16:26:14 +0545

    api addded

--- src/app/(auth)/login/Login.js
+++ src/app/(auth)/login/Login.js
@@ -3,7 +3,7 @@
 import axiosInstance from "@/app/utils/axiosInstance";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-
+const img = "/assets/image/logo.jpg";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -41,27 +41,29 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-sm mx-auto mt-20 border  p-10 rounded-md">
+    <form onSubmit={handleSubmit} className="max-w-sm mx-auto mt-20 border font-[karla]  p-10 rounded-md">
+        <div className="h-44 flex justify-center  "><img src={img} className="h-44"></img></div>
       <div className="mb-5">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label htmlFor="email" className="block my-2 text-sm font-medium text-gray-600 dark:text-white">
           Your email
         </label>
         <input
           type="email"
           id="email"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@flowbite.com"
+          placeholder="Enter your Email "
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
       </div>
       <div className="mb-5">
-        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">
           Your password
         </label>
         <input
           type="password"
+          placeholder="Enter your Password "
           id="password"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={password}
@@ -69,23 +71,10 @@ const Login = () => {
           required
         />
       </div>
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
-          <input
-            id="remember"
-            type="checkbox"
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            checked={rememberMe}
-            onChange={(e) => setRememberMe(e.target.checked)}
-          />
-        </div>
-        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-          Remember me
-        </label>
-      </div>
+      
       <button
         type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white flex justify-center items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Login
       </button>
--- src/app/(platform)/component/HomeDesignPart.js
+++ src/app/(platform)/component/HomeDesignPart.js
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 import { useState } from "react";
-import'../component/servicecontent/index.css'
+import "../component/servicecontent/index.css";
 import axiosInstance from "@/app/utils/axiosInstance";
 import { useEffect } from "react";
+import Link from "next/link";
 function HomeDesignPart() {
   const [data, setData] = useState([]);
   const [data2, setData2] = useState([]);
@@ -10,55 +11,55 @@ function HomeDesignPart() {
   const [headerdata, setheaderdata] = useState([]);
   const fetchData1 = async () => {
     try {
-      const response = await axiosInstance.get('/api/settings');
+      const response = await axiosInstance.get("/api/settings");
       if (response.data.success) {
         setheaderdata(response.data.data.data); // Update state with fetched data
       } else {
-        console.error('Failed to fetch data');
+        console.error("Failed to fetch data");
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
   const fetchData = async () => {
     try {
-      const response = await axiosInstance.get('/api/pages/9');
+      const response = await axiosInstance.get("/api/pages/9");
       if (response.data.success) {
         setData(response.data.data); // Update state with fetched data
       } else {
-        console.error('Failed to fetch data');
+        console.error("Failed to fetch data");
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
   const fetchData2 = async () => {
     try {
-      const response = await axiosInstance.get('/api/pages/10');
+      const response = await axiosInstance.get("/api/pages/10");
       if (response.data.success) {
         setData2(response.data.data); // Update state with fetched data
       } else {
-        console.error('Failed to fetch data');
+        console.error("Failed to fetch data");
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
   const fetchData3 = async () => {
     try {
-      const response = await axiosInstance.get('/api/pages/11');
+      const response = await axiosInstance.get("/api/pages/11");
       if (response.data.success) {
         setData3(response.data.data); // Update state with fetched data
       } else {
-        console.error('Failed to fetch data');
+        console.error("Failed to fetch data");
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
   useEffect(() => {
     // Function to fetch data from APi
-    fetchData(); 
+    fetchData();
     fetchData2();
     fetchData3();
     fetchData1();
@@ -66,62 +67,99 @@ function HomeDesignPart() {
   }, []); // Empty dependency array ensures that this effect runs only once when the component mounts
   return (
     <>
-      <div className='bg-customgray py-16 font-[karla]'>
+      <div className="bg-customgray py-16 font-[karla]">
         {/* Header */}
-        <div className='font-karla'>
-          <p className='text-brown md:text-[18px] text-[15px] leading-[24px] font-[400] grid place-content-center'>Design & construction.</p>
-          <p className='text-customblue md:text-[36px] text-[26px] leading-[49px] md:font-[700] font-[700] my-1 grid place-content-center'>{headerdata.design_section_description}</p>
+        <div className="font-karla">
+          <p className="text-brown md:text-[18px] text-[15px] leading-[24px] font-[400] grid place-content-center">
+            Design & construction.
+          </p>
+          <p className="text-customblue md:text-[36px] text-[26px] leading-[49px] md:font-[700] font-[700] my-1 grid place-content-center">
+            {headerdata.design_section_description}
+          </p>
         </div>
 
         {/* Images */}
-        <div className='mx-auto container grid md:grid-cols-3 md:px-0 px-4 gap-4'>
-        
-        <div className="image-container relative h-[350px]">
-    <div className="bg-black w-full flex justify-center items-center relative">
-        {/* Overlay to darken the image */}
-        <div className='w-full h-full absolute top-0 left-0 bg-black opacity-70'  style={{ backgroundColor: '#051721', opacity: '0.7', zIndex: '1'}}></div>
-        <img 
-            src={axiosInstance.defaults.baseURL + data.image} 
-            alt='img1' 
-            className="img-transition h-[350px]"
-            style={{ transition: 'transform 0.3s ease', zIndex: '0' }}
-        />
-    </div>
-    {/* Title with improved visibility */}
-    <p className="absolute inset-0 flex items-center justify-center text-white text-[24px] leading-[32px] font-[600] z-10">{data.title}</p>
-</div>
-{/* for dual */}
-<div className="image-container relative h-[350px]">
-    <div className="bg-black w-full flex justify-center items-center relative">
-        {/* Overlay to darken the image */}
-        <div className='w-full h-full absolute top-0 left-0 bg-black opacity-70'  style={{ backgroundColor: '#051721', opacity: '0.7', zIndex: '1'}}></div>
-        <img 
-            src={axiosInstance.defaults.baseURL + data2.image} 
-            alt='img1' 
-            className="img-transition h-[350px]"
-            style={{ transition: 'transform 0.3s ease', zIndex: '0' }}
-        />
-    </div>
-    {/* Title with improved visibility */}
-    <p className="absolute inset-0 flex items-center justify-center text-white text-[24px] leading-[32px] font-[600] z-10">{data2.title}</p>
-</div>
-{/* for dual occupanc */}
-<div className="image-container relative h-[350px]">
-    <div className="bg-black w-full flex justify-center items-center relative">
-        {/* Overlay to darken the image */}
-        <div className='w-full h-full absolute top-0 left-0 bg-black opacity-70'  style={{ backgroundColor: '#051721', opacity: '0.7', zIndex: '1'}}></div>
-        <img 
-            src={axiosInstance.defaults.baseURL + data3.image} 
-            alt='img1' 
-            className="img-transition h-[350px]"
-            style={{ transition: 'transform 0.3s ease', zIndex: '0' }}
-        />
-    </div>
-    {/* Title with improved visibility */}
-    <p className="absolute inset-0 flex items-center justify-center text-white text-[24px] leading-[32px] font-[600] z-10">{data3.title}</p>
-</div>
- 
+        <div className="mx-auto container grid md:grid-cols-3 md:px-0 px-4 gap-4">
+          {/* for img part1 */}
+          <Link href="/design/singlehome">
+            <div className="image-container relative h-[350px]">
+              <div className="bg-black w-full flex justify-center items-center relative">
+                {/* Overlay to darken the image */}
+                <div
+                  className="w-full h-full absolute top-0 left-0 bg-black opacity-70"
+                  style={{
+                    backgroundColor: "#051721",
+                    opacity: "0.7",
+                    zIndex: "1",
+                  }}
+                ></div>
+                <img
+                  src={axiosInstance.defaults.baseURL + data.image}
+                  alt="img1"
+                  className="img-transition h-[350px]"
+                  style={{ transition: "transform 0.3s ease", zIndex: "0" }}
+                />
+              </div>
+              {/* Title with improved visibility */}
+              <p className="absolute inset-0 flex items-center justify-center text-white text-[24px] leading-[32px] font-[600] z-10">
+                {data.title}
+              </p>
+            </div>
+          </Link>
 
+          {/* for dual */}
+          <Link href="/design/doublehome">
+            <div className="image-container relative h-[350px]">
+              <div className="bg-black w-full flex justify-center items-center relative">
+                {/* Overlay to darken the image */}
+                <div
+                  className="w-full h-full absolute top-0 left-0 bg-black opacity-70"
+                  style={{
+                    backgroundColor: "#051721",
+                    opacity: "0.7",
+                    zIndex: "1",
+                  }}
+                ></div>
+                <img
+                  src={axiosInstance.defaults.baseURL + data2.image}
+                  alt="img1"
+                  className="img-transition h-[350px]"
+                  style={{ transition: "transform 0.3s ease", zIndex: "0" }}
+                />
+              </div>
+              {/* Title with improved visibility */}
+              <p className="absolute inset-0 flex items-center justify-center text-white text-[24px] leading-[32px] font-[600] z-10">
+                {data2.title}
+              </p>
+            </div>
+          </Link>
+
+          {/* for dual occupanc */}
+          <Link href='/design/doubleoccupance'>
+            <div className="image-container relative h-[350px]">
+              <div className="bg-black w-full flex justify-center items-center relative">
+                {/* Overlay to darken the image */}
+                <div
+                  className="w-full h-full absolute top-0 left-0 bg-black opacity-70"
+                  style={{
+                    backgroundColor: "#051721",
+                    opacity: "0.7",
+                    zIndex: "1",
+                  }}
+                ></div>
+                <img
+                  src={axiosInstance.defaults.baseURL + data3.image}
+                  alt="img1"
+                  className="img-transition h-[350px]"
+                  style={{ transition: "transform 0.3s ease", zIndex: "0" }}
+                />
+              </div>
+              {/* Title with improved visibility */}
+              <p className="absolute inset-0 flex items-center justify-center text-white text-[24px] leading-[32px] font-[600] z-10">
+                {data3.title}
+              </p>
+            </div>
+          </Link>
         </div>
       </div>
     </>
