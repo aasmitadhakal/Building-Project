97c0c70513add4300c934ac494be00d65db9e77f
aasmitadhakal <dhakalreshma52@gmail.com>
2024-04-07 13:22:28 +0545

    error edited

--- src/app/(platform)/component/designcontent/Singlestoreyhomes.js
+++ src/app/(platform)/component/designcontent/Singlestoreyhomes.js
@@ -34,20 +34,22 @@ function Singlestoreyhomes() {
 
 
 {/* for cards */}
-     <section className="grid md:grid-cols-3 grid-cols-1 md:px-0 px-4 container mx-auto md:gap-8 mb-36 mt-12 font-[Montserrat]">
-      {data.map((property, index) => (
-        <Link href={`/design/singlehome/${property.id}`} key={index} className=" shadow-xl p-4 ">
+<section className="grid md:grid-cols-3 grid-cols-1 md:px-0 px-4 container mx-auto md:gap-8 mb-36 mt-12 font-[Montserrat]">
+        {data.map((property, index) => (
+          <Link href={`/design/singlehome/${property.id}`} key={index}>
+            <div className="shadow-xl p-4">
 
-             <h3 className='text-customblue  font-[600] text-[24px] leading-[35px] mx-2'>{property.title}</h3>
-          <img src={axiosInstance.defaults.baseURL + property.image} alt={property.title} className='h-[241px] w-full mb-4' />
-         
-          <p className='text-black  font-[400] text-[16px] leading-[22px] my-1 mx-2 flex'>Frontage Size: {property.frontage}</p>
-          <p className='text-black  font-[400] text-[16px] leading-[22px] my-1 flex'><FaBed  className='text-customblue mx-2'/> {property.bedroom}<p className='mx-1'>Bedrooms</p></p>
-          <p className='text-black  font-[400] text-[16px] leading-[22px] my-1 flex'> <FaCar className='text-customblue mx-2'/> {property.cars}<p className='mx-1'>Cars</p></p>
-          <p className='text-black  font-[400] text-[16px] leading-[22px] my-1 flex '><FaToilet className='text-customblue mx-2' /> {property.bathrooms}<p className='mx-1'>Bathroom</p></p>
-        </Link>
-      ))}
-    </section>
+              <h3 className='text-customblue font-[600] text-[24px] leading-[35px] mx-2'>{property.title}</h3>
+              <img src={axiosInstance.defaults.baseURL + property.image} alt={property.title} className='h-[241px] w-full mb-4' />
+
+              <p className='text-black font-[400] text-[16px] leading-[22px] my-1 mx-2 flex'>Frontage Size: {property.frontage}</p>
+              <p className='text-black font-[400] text-[16px] leading-[22px] my-1 flex'><FaBed className='text-customblue mx-2'/> {property.bedroom}<span className='mx-1'>Bedrooms</span></p>
+              <p className='text-black font-[400] text-[16px] leading-[22px] my-1 flex'> <FaCar className='text-customblue mx-2'/> {property.cars}<span className='mx-1'>Cars</span></p>
+              <p className='text-black font-[400] text-[16px] leading-[22px] my-1 flex '><FaToilet className='text-customblue mx-2' /> {property.bathrooms}<span className='mx-1'>Bathroom</span></p>
+            </div>
+          </Link>
+        ))}
+      </section>
     </>
    
   );
