77d5ed1d2befa122ebb0c4e445077526f26faaf4
Harish Pandey <pandeyharish394@gmail.com>
2024-04-08 16:06:33 +0545

    fixed: bg of login page

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,12 +1,11 @@
-"use client";
-
-import bg from "/public/assets/image/78786.jpg";
 import "remixicon/fonts/remixicon.css";
+
+const loginbg = "/assets/image/loginbg.jpg"; // Updated import path
 export default function AuthLayout({
   children, // will be a page or nested layout
 }) {
   return (
-    <div className="min-h-screen bg-gray-50 " style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover", backgroundPosition: "center" }}>
+    <div className="min-h-screen bg-gray-50" style={{ backgroundImage: `url(${loginbg})`, backgroundSize: "cover", backgroundPosition: "center" }}>
       {/* This is the layout for auth pages  */}
       {children}
     </div>
